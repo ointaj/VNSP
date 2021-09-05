3c6d6b89147134f6e2a07dba30426521fee3e695
ointaj <sintaj.oliver1@gmail.com>
2021-09-05 21:07:11 +0200

    Step(21) Changed: changed storage system

--- src/storage.py
+++ src/storage.py
@@ -8,7 +8,16 @@ class cFileMethods:
         self.Read = "r"
 
 
-class cBStroage:
+class cFileExtension:
+    def __init__(self):
+        self.TxtExt = ".txt"
+        self.CsvExt = ".csv"
+
+
+class cBStorage:
+
+    def FileNameCheck(self):
+        pass
 
     def CreateFolder(self):
         pass
@@ -17,25 +26,48 @@ class cBStroage:
         pass
 
 
-class cStorage(cBStroage):
+class cStorage(cBStorage):
     def __init__(self, _sFileName, _sDelimeter):
         self.sFileName = _sFileName
         self.sDelimeter = _sDelimeter
 
         self.CurrentDirectory = os.getcwd()
+        self.sDirName = 'Storage/'
         self.NewDir = os.path.join(self.CurrentDirectory, r'Storage')
 
         self.bAppendFlag = True
 
         self.oFileMethods = cFileMethods()
+        self.oFileExtension = cFileExtension()
+
+    def FileNameCheck(self):
+        PosOfExt = self.sFileName.find(".")
+        if PosOfExt != -1:
+            if self.sFileName[PosOfExt:len(self.sFileName)] == self.oFileExtension.TxtExt:
+                return True
+            else:
+                return False
+        else:
+            return False
 
     def CreateFolder(self):
         if not os.path.exists(self.NewDir):
-            os.makedirs(self.NewDir)
+            try:
+                os.makedirs(self.NewDir)
+            except:
+                print("Error of creating storage for game !")
+                exit()
 
     def CreateFile(self, _sScore, _sPlayerName):
-        file = open(self.sFileName, self.oFileMethods.Append)
-        file.write(_sScore + self.sDelimeter + _sPlayerName + '\n')
-        file.close()
+        if self.FileNameCheck():
+            try:
+                file = open(self.sDirName + self.sFileName, self.oFileMethods.Append)
+                try:
+                    file.write(_sScore + self.sDelimeter + _sPlayerName + '\n')
+                except:
+                    print("Can't write to file")
+                finally:
+                    file.close()
+            except:
+                print("File can't be open")
         self.bAppendFlag = False
-
